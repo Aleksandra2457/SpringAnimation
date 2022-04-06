9589ba48ff058ee6390c1a19d924510230e919c8
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-04-06 12:57:38 +0300

    Animation properties were changed.

--- SpringAnimation/Models/Animation.swift
+++ SpringAnimation/Models/Animation.swift
@@ -20,9 +20,9 @@ struct Animation {
         Animation(
             preset: Spring.AnimationPreset.allCases.randomElement() ?? .fadeIn,
             curve: Spring.AnimationCurve.allCases.randomElement() ?? .easeIn,
-            force: Double.random(in: 0.2...0.5),
-            duration: Double.random(in: 1...3),
-            delay: Double.random(in: 0.5...1.2))
+            force: Double.random(in: 0.2...0.8),
+            duration: Double.random(in: 0.5...1.5),
+            delay: Double.random(in: 0.2...0.6))
     }
     
 }
